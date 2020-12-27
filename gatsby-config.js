441c18b8d4ae73d86a8e56847fd4d456c12c7000
Thibault Friedrich <thibault.friedrich@gmail.com>
2020-12-27 18:02:58 +0100

    build: fix gatsby config for sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'website',
+    title: 'Research, Design, Code',
+    siteUrl: 'https://thibaultfriedrich.github.io/website',
   },
   plugins: [
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -69,164 +69,116 @@ const methods = [
   },
 ]
 
-const socials = [
-  {
-    icon: '',
-    label: 'email',
-  },
-  {
-    icon: '',
-    label: 'linkedin',
-  },
-  {
-    icon: '',
-    label: 'medium',
-  },
-  {
-    icon: '',
-    label: 'github',
-  },
-]
 const email = 'thibault.friedrich@gmail.com'
 const link = `mailto:${email}`
 
-const IndexPage = () => {
-  const [emailDisplayed, showEmail] = useState(false)
-
-  const timeout = useRef()
-
-  const clear = () => {
-    if (timeout.current) {
-      clearTimeout(timeout.current)
-      timeout.current = null
-    }
-  }
-
-  useEffect(() => {
-    return clear
-  }, [])
-
-  const onClick = () => {
-    showEmail(true)
-    clear()
-
-    timeout.current = setTimeout(() => {
-      showEmail(false)
-      clear()
-    }, 10000)
-  }
-
-  return (
-    <Layout>
-      <Section className={style.Welcome}>
-        <h1 className={style.title}>
-          <div className={style.titleBorder}>
-            Research
-            <br />
-            Design
-            <br />
-            Develop
-          </div>
-        </h1>
-        <h2 className={style.subTitle}>
-          A cutting-edge design and development agency
-          <br /> to make your digital solution unforgettable
-        </h2>
-      </Section>
-      <Section className={style.Reason}>
-        <div className={style.content}>
-          <p>
-            Digital users are very solicited with a lot of existing websites and
-            applications. It is hard to find a place for new digital products.
-          </p>
-          <p>
-            <strong>
-              Make the difference with a customized, smooth and unique user
-              experience.
-            </strong>
-          </p>
+const IndexPage = () => (
+  <Layout>
+    <Section className={style.Welcome}>
+      <h1 className={style.title}>
+        <div className={style.titleBorder}>
+          Research
+          <br />
+          Design
+          <br />
+          Develop
         </div>
-      </Section>
-      <Section className={style.Technologies}>
-        <div className={style.content}>
-          <h3 className={style.title}>
-            We design & develop unique user experiences using cutting-edge
-            technologies
-          </h3>
+      </h1>
+      <h2 className={style.subTitle}>
+        A cutting-edge design and development agency
+        <br /> to make your digital solution unforgettable
+      </h2>
+    </Section>
+    <Section className={style.Reason}>
+      <div className={style.content}>
+        <p>
+          Digital users are very solicited with a lot of existing websites and
+          applications. It is hard to find a place for new digital products.
+        </p>
+        <p>
+          <strong>
+            Make the difference with a customized, smooth and unique user
+            experience.
+          </strong>
+        </p>
+      </div>
+    </Section>
+    <Section className={style.Technologies}>
+      <div className={style.content}>
+        <h3 className={style.title}>
+          We design & develop unique user experiences using cutting-edge
+          technologies
+        </h3>
+        <div className={style.list}>
+          {technologies.map(t => (
+            <div className={style.Technology} key={t.label}>
+              <img src={t.icon} alt={t.label} className={style.icon} />
+              <div className={style.name}>{t.label}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Section>
+    <Section className={style.Benefits}>
+      <div className={style.content}>
+        <h3 className={style.title}>Strong Benefits</h3>
+        <div className={style.list}>
+          {benefits.map(b => (
+            <div className={style.Benefit} key={b.label}>
+              <div className={style.label}>{b.label}</div>
+              <div className={style.conclusion}>
+                <strong>{b.conclusion}</strong>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Section>
+    <Section className={style.Mission}>
+      <div className={style.content}>
+        <h3 className={style.title}>Mission</h3>
+        <div className={style.paragraph}>
+          We are a team of highly skilled passionate designers and developers
+          always ready for new challenges:
           <div className={style.list}>
-            {technologies.map(t => (
-              <div className={style.Technology} key={t.label}>
-                <img src={t.icon} alt={t.label} className={style.icon} />
-                <div className={style.name}>{t.label}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </Section>
-      <Section className={style.Benefits}>
-        <div className={style.content}>
-          <h3 className={style.title}>Strong Benefits</h3>
-          <div className={style.list}>
-            {benefits.map(b => (
-              <div className={style.Benefit} key={b.label}>
-                <div className={style.label}>{b.label}</div>
-                <div className={style.conclusion}>
-                  <strong>{b.conclusion}</strong>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </Section>
-      <Section className={style.Mission}>
-        <div className={style.content}>
-          <h3 className={style.title}>Mission</h3>
-          <div className={style.paragraph}>
-            We are a team of highly skilled passionate designers and developers
-            always ready for new challenges:
-            <div className={style.list}>
-              <div className={style.item}>
-                Design and implement digital solutions from scratch for
-                production
-              </div>
-              <div className={style.item}>Fast prototype proofs of concept</div>
-              <div className={style.item}>
-                Train your dev team with innovate Human-Computer Interfaces
-                technologies
-              </div>
+            <div className={style.item}>
+              Design and implement digital solutions from scratch for production
+            </div>
+            <div className={style.item}>Fast prototype proofs of concept</div>
+            <div className={style.item}>
+              Train your dev team with innovate Human-Computer Interfaces
+              technologies
             </div>
           </div>
         </div>
-      </Section>
-      <Section className={style.Methods}>
-        <div className={style.content}>
-          <h3 className={style.title}>Methodology</h3>
-          <p>5 key points to deliver you a great solution</p>
-          <div className={style.list}>
-            {methods.map(method => (
-              <div className={style.Method} key={method.label}>
-                <div className={style.label}>{method.label}</div>
-                <div className={style.description}>{method.description}</div>
-              </div>
-            ))}
-          </div>
+      </div>
+    </Section>
+    <Section className={style.Methods}>
+      <div className={style.content}>
+        <h3 className={style.title}>Methodology</h3>
+        <p>5 key points to deliver you a great solution</p>
+        <div className={style.list}>
+          {methods.map(method => (
+            <div className={style.Method} key={method.label}>
+              <div className={style.label}>{method.label}</div>
+              <div className={style.description}>{method.description}</div>
+            </div>
+          ))}
         </div>
-      </Section>
-      <Section className={style.Contact}>
-        <div className={style.content}>
-          <h3 className={style.title}>Contact</h3>
-          <p>
-            You have a new challenge for us? You want to be part of the team?
-          </p>
-          <div className={style.callToAction}>
-            <a className={style.link} href={link}>
-              {email}
-            </a>
-          </div>
+      </div>
+    </Section>
+    <Section className={style.Contact}>
+      <div className={style.content}>
+        <h3 className={style.title}>Contact</h3>
+        <p>You have a new challenge for us? You want to be part of the team?</p>
+        <div className={style.callToAction}>
+          <a className={style.link} href={link}>
+            {email}
+          </a>
         </div>
-      </Section>
-    </Layout>
-  )
-}
+      </div>
+    </Section>
+  </Layout>
+)
 
 export default IndexPage
