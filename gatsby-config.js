cd160a076c590a4e627c82c7bef17099c6f057ef
Thibault Friedrich <thibault.friedrich@gmail.com>
2021-02-26 11:17:47 -0500

    chore: fix google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,9 +18,19 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'G-MKT9P50583',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ['G-Q7S8TZPM5N'],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
       },
     },
     'gatsby-plugin-sharp',
