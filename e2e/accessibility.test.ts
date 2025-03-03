ad978af451468caa42e0d04a70c1bcaf61d4673a
Thibault Friedrich <thibault.friedrich@gmail.com>
2025-03-03 18:27:24 -0500

    test: fix e2e accessibility issues

--- e2e/accessibility.test.ts
+++ e2e/accessibility.test.ts
@@ -1,9 +1,14 @@
 import { test, expect } from '@playwright/test'
 import { injectAxe, checkA11y } from 'axe-playwright'
 
-test('should show the templates section', async ({ page }) => {
+test('should not have accessibility violations', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/')
-  // Find an element with the text 'About Page' and click on it
-  await expect(page.getByText('Templates')).toBeVisible()
+
+  await injectAxe(page)
+
+  await checkA11y(page, {
+    detailedReport: true,
+    detailedReportOptions: { html: true },
+  })
 })
