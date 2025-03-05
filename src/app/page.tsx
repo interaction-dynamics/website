cebce3c116f9d48a89dc70b5dcecd7de1d4a8682
Thibault Friedrich <thibault.friedrich@gmail.com>
2025-03-05 07:19:59 -0500

    feat: add flags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import Image from 'next/image'
 import { LanguageSelector } from '../components/language-selector'
 import { getTranslation } from '@/services/i18n/server'
 import { DarkModeToggle } from '@/components/dark-mode-toggle'
+import { showDeploy } from '@/flags'
 
 export default async function Index() {
   const { t } = await getTranslation()
+
+  const deploy = await showDeploy()
 
   return (
     <div className="flex min-h-screen flex-col items-stretch">
@@ -103,22 +106,25 @@ export default async function Index() {
             </p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Deploy{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-70`}>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
-          </a>
+          {deploy && (
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Deploy{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-70`}>
+                Instantly deploy your Next.js site to a shareable URL with
+                Vercel.
+              </p>
+            </a>
+          )}
         </div>
       </main>
     </div>
