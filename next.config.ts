5cca2ba03372f1fde5604e1de34574bf16afd50f
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-03-07 16:56:24 -0500

    feat: support markdown (#24)

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import { NextConfig } from 'next'
+import createMDX from '@next/mdx'
 import WithVercelToolbar from '@vercel/toolbar/plugins/next'
 
 const withVercelToolbar = WithVercelToolbar()
 
 const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   experimental: {
     authInterrupts: true,
   },
 }
 
-export default withVercelToolbar(nextConfig)
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+export default withMDX(withVercelToolbar(nextConfig))
