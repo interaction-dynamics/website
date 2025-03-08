da1d7774d8bc8d7fa4f9349c879f55f29195c469
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-03-08 17:23:40 -0500

    feat: more style for typography (#29)

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -5,6 +5,11 @@ export const typographyVariants = cva('', {
   variants: {
     variant: {
       h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+      h2: 'mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0',
+      h3: 'mt-8 scroll-m-20 text-2xl font-semibold tracking-tight',
+      p: 'leading-7 [&:not(:first-child)]:mt-6',
+      blockquote: 'mt-6 border-l-2 pl-6 italic',
+      ul: 'my-6 ml-6 list-disc [&>li]:mt-2',
     },
   },
   defaultVariants: {
--- src/services/content-management/markdown/mdx-components.tsx
+++ src/services/content-management/markdown/mdx-components.tsx
@@ -5,6 +5,9 @@ import { Typography } from '@/components/typography'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => <Typography variant="h1">{children}</Typography>,
+    h2: ({ children }) => <Typography variant="h2">{children}</Typography>,
+    h3: ({ children }) => <Typography variant="h3">{children}</Typography>,
+    p: ({ children }) => <Typography variant="p">{children}</Typography>,
     img: (props) => (
       <Image
         sizes="100vw"
