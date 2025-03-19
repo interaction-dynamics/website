20ae25f6bb32f590ede7f3c64f55a5d10b48297d
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-03-19 07:15:34 -0400

    refactor: remove clerk

--- src/app/(main)/(protected)/README.md
+++
@@ -1,5 +0,0 @@
-# Protected Routes
-
-All these routes are protected and required to be authenticated to access them.
-
-When you add a route, you should update the file [protected-routes.tsx](./protected-routes.tsx) to add the route to the list of protected routes.
--- src/app/(main)/(protected)/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Section } from '@/components/section'
-import { getTranslations } from '@/services/translation'
-
-export default async function DashboardPage() {
-  const { t } = await getTranslations('dashboard')
-
-  return <Section title={t('title')}></Section>
-}
