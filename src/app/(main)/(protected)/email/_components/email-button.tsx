14f9e866470a10fde141a2c6343aec191867591c
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-03-19 07:15:34 -0400

    refactor: remove clerk

--- src/app/(main)/(protected)/email/_components/email-button.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import { sendTestEmail } from '../_use-cases/send-test-email'
-
-export function EmailButton() {
-  return <Button onClick={() => sendTestEmail()}>Send Email</Button>
-}
--- src/app/(main)/(protected)/protected-routes.tsx
+++
@@ -1,4 +0,0 @@
-/**
- * Update this list when you want to add more protected routes
- */
-export const protectedRoutes = ['/checkout(.*)', '/dashboard(.*)', '/email(.*)']
