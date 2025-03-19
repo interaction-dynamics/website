d2b8f70c381daac3d133ac7d03ad10da1eda9525
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-03-19 08:23:53 -0400

    feat: add layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,5 @@
-import { LanguageSelector } from '@/components/language-selector'
-import { DarkModeToggle } from '@/components/dark-mode-toggle'
-import { FeedbackButton } from '@/components/feedback-button'
-import {
-  SignInButton,
-  SignedOut,
-  SignUpButton,
-  SignedIn,
-  UserButton,
-} from '@/services/authentication'
 import { Footer } from '@/components/sitemap-footer'
+import { Header } from './(home)/_components/header'
 
 interface MainLayoutProps {
   children: React.ReactNode
@@ -17,18 +8,7 @@ interface MainLayoutProps {
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col items-stretch">
-      <nav className="flex flex-row justify-end items-stretch gap-4 p-4 sticky top-0">
-        <FeedbackButton />
-        <DarkModeToggle />
-        <LanguageSelector />
-        <SignedOut>
-          <SignInButton variant="outline" forceRedirectUrl="/dashboard" />
-          <SignUpButton forceRedirectUrl="/dashboard" />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </nav>
+      <Header />
       <div className="flex-1">{children}</div>
       <Footer />
     </div>
