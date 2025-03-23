302fdac9d7bd2963de07b22b6c158ba07ff6fddf
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-03-22 21:30:21 -0400

    feat: add translation param (#46)
    * feat: add translation param
    
    * refactor: fix types

--- src/services/translation/i18n/client.tsx
+++ src/services/translation/i18n/client.tsx
@@ -2,6 +2,7 @@
 import { useContext } from 'react'
 import { changeLocale } from '@/services/translation'
 import { TranslationContext } from './_private/context'
+import { Parameters } from '../_types/parameters'
 
 export function useTranslations(namespace?: string) {
   const { locale, t, languages } = useContext(TranslationContext)
@@ -9,8 +10,8 @@ export function useTranslations(namespace?: string) {
   return {
     locale,
     languages,
-    t: (key: string, specificNamespace?: string) =>
-      t(key, specificNamespace ?? namespace),
+    t: (key: string, parameters: Parameters = {}, specificNamespace?: string) =>
+      t(key, parameters, specificNamespace ?? namespace),
     changeLocale,
   }
 }
